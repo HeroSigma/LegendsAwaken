4d3c94670f8a6e0c925f562b60c589e333ccdd58
HeroSigma <68994426+HeroSigma@users.noreply.github.com>
2025-07-14 00:33:07 -0500

    Update quests.h

--- include/quests.h
+++ include/quests.h
@@ -1,7 +1,7 @@
 #ifndef GUARD_QUESTS_H
 #define GUARD_QUESTS_H
 
-#include constants/quests.h       //included in global.h
+#include "constants/quests.h"       //included in global.h
 
 #define SORT_DEFAULT 0
 #define SORT_INACTIVE 1
