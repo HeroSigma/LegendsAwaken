aee565a9d63ed450a33510728aeacaf6b1e97878
HeroSigma <68994426+HeroSigma@users.noreply.github.com>
2025-08-28 06:27:42 -0500

    fix: remove duplicate setspeaker implementation

--- include/strings.h
+++ include/strings.h
@@ -287,6 +287,8 @@ extern const u8 gText_Birch_WhatsYourName[];
 extern const u8 gText_Birch_SoItsPlayer[];
 extern const u8 gText_Birch_YourePlayer[];
 extern const u8 gText_Birch_AreYouReady[];
+extern const u8 gText_BirchSpeaker[];
+extern const u8 gText_PlayerSpeaker[];
 
 extern const u8 gText_BirchInTrouble[];
 extern const u8 gText_ConfirmStarterChoice[];
