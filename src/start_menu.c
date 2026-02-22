17003c24ee0982277b0ba22b2ad4594441c227e5
HeroSigma <xenoacemods@gmail.com>
2026-02-22 10:13:52 -0500

    Move Type: Null event to RustboroBasement2 and fix Green event triggering
    - Move Type: Null (Silvally) event from Devon Corp 3F to RustboroBasement2 at X3 Y5
    - Fix Green event to trigger after receiving Pokénav (VAR_DEVON_CORP_3F_STATE == 1)
    - Remove Type: Null references from Devon Corp 3F scripts and map
    - Add LOCALID_TYPE_NULL constant for RustboroBasement2
    - Use correct species graphics format for Type: Null encounter
    - Clean up unused Type: Null scripts from Devon Corp 3F

--- src/start_menu.c
+++ src/start_menu.c
@@ -65,7 +65,7 @@ enum
     MENU_ACTION_POKENAV,
     MENU_ACTION_PLAYER,
     MENU_ACTION_SAVE,
-    MENU_ACTION_STORE,
+    // MENU_ACTION_STORE,  // Removed - Now using Slateport NPC instead
     MENU_ACTION_OPTION,
     MENU_ACTION_EXIT,
     MENU_ACTION_RETIRE_SAFARI,
@@ -112,7 +112,7 @@ EWRAM_DATA static u8 sSaveInfoWindowId = 0;
 static bool8 StartMenuPokedexCallback(void);
 static bool8 StartMenuPokemonCallback(void);
 static bool8 StartMenuBagCallback(void);
-static bool8 StartMenuStoreCallback(void);
+// static bool8 StartMenuStoreCallback(void);  // Removed - Now using Slateport NPC instead
 static bool8 StartMenuPokeNavCallback(void);
 static bool8 StartMenuPlayerNameCallback(void);
 static bool8 StartMenuSaveCallback(void);
@@ -214,7 +214,7 @@ static const struct MenuAction sStartMenuItems[] =
     [MENU_ACTION_POKENAV]         = {gText_MenuPokenav, {.u8_void = StartMenuPokeNavCallback}},
     [MENU_ACTION_PLAYER]          = {gText_MenuPlayer,  {.u8_void = StartMenuPlayerNameCallback}},
     [MENU_ACTION_SAVE]            = {gText_MenuSave,    {.u8_void = StartMenuSaveCallback}},
-    [MENU_ACTION_STORE]           = {sText_MenuStore,   {.u8_void = StartMenuStoreCallback}},
+    // [MENU_ACTION_STORE]           = {sText_MenuStore,   {.u8_void = StartMenuStoreCallback}},  // Removed - Now using Slateport NPC instead
     [MENU_ACTION_OPTION]          = {gText_MenuOption,  {.u8_void = StartMenuOptionCallback}},
     [MENU_ACTION_EXIT]            = {gText_MenuExit,    {.u8_void = StartMenuExitCallback}},
     [MENU_ACTION_RETIRE_SAFARI]   = {gText_MenuRetire,  {.u8_void = StartMenuSafariZoneRetireCallback}},
@@ -381,7 +381,7 @@ static void BuildNormalStartMenu(void)
 
     AddStartMenuAction(MENU_ACTION_PLAYER);
     
-    AddStartMenuAction(MENU_ACTION_STORE);
+    // AddStartMenuAction(MENU_ACTION_STORE);  // Removed - Now using Slateport NPC instead
     AddStartMenuAction(MENU_ACTION_SAVE);
     AddStartMenuAction(MENU_ACTION_QUEST_MENU);
     AddStartMenuAction(MENU_ACTION_OPTION);
@@ -399,7 +399,7 @@ static void BuildDebugStartMenu(void)
     if (FlagGet(FLAG_SYS_POKENAV_GET) == TRUE)
         AddStartMenuAction(MENU_ACTION_POKENAV);
     AddStartMenuAction(MENU_ACTION_PLAYER);
-    AddStartMenuAction(MENU_ACTION_STORE);
+    // AddStartMenuAction(MENU_ACTION_STORE);  // Removed - Now using Slateport NPC instead
     AddStartMenuAction(MENU_ACTION_SAVE);
     AddStartMenuAction(MENU_ACTION_QUEST_MENU);
     AddStartMenuAction(MENU_ACTION_OPTION);
@@ -412,7 +412,7 @@ static void BuildSafariZoneStartMenu(void)
     AddStartMenuAction(MENU_ACTION_POKEMON);
     AddStartMenuAction(MENU_ACTION_BAG);
     AddStartMenuAction(MENU_ACTION_PLAYER);
-    AddStartMenuAction(MENU_ACTION_STORE);
+    // AddStartMenuAction(MENU_ACTION_STORE);  // Removed - Now using Slateport NPC instead
     AddStartMenuAction(MENU_ACTION_QUEST_MENU);
     AddStartMenuAction(MENU_ACTION_OPTION);
     AddStartMenuAction(MENU_ACTION_EXIT);
@@ -430,7 +430,7 @@ static void BuildLinkModeStartMenu(void)
 
     AddStartMenuAction(MENU_ACTION_PLAYER_LINK);
     AddStartMenuAction(MENU_ACTION_QUEST_MENU);
-    AddStartMenuAction(MENU_ACTION_STORE);
+    // AddStartMenuAction(MENU_ACTION_STORE);  // Removed - Now using Slateport NPC instead
     AddStartMenuAction(MENU_ACTION_OPTION);
     AddStartMenuAction(MENU_ACTION_EXIT);
 }
@@ -447,7 +447,7 @@ static void BuildUnionRoomStartMenu(void)
 
     AddStartMenuAction(MENU_ACTION_PLAYER);
     AddStartMenuAction(MENU_ACTION_QUEST_MENU);
-    AddStartMenuAction(MENU_ACTION_STORE);
+    // AddStartMenuAction(MENU_ACTION_STORE);  // Removed - Now using Slateport NPC instead
     AddStartMenuAction(MENU_ACTION_OPTION);
     AddStartMenuAction(MENU_ACTION_EXIT);
 }
@@ -810,6 +810,7 @@ static bool8 StartMenuBagCallback(void)
     return FALSE;
 }
 
+/*  // Removed - Now using Slateport NPC instead
 static bool8 StartMenuStoreCallback(void)
 {
     if (!gPaletteFade.active)
@@ -822,6 +823,7 @@ static bool8 StartMenuStoreCallback(void)
     }
     return FALSE;
 }
+*/
 
 static bool8 StartMenuPokeNavCallback(void)
 {
